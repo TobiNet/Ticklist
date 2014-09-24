8d7a5f6f59f69fe24e3398c9eacce68720908785
TobiNet <tobiasmro@googlemail.com>
2014-09-24 23:54:53 +0200

    Software Layer Type set for correct color blending

--- src/org/tobinet/tick/ColorChooser/AmbilWarnaKotak.java
+++ src/org/tobinet/tick/ColorChooser/AmbilWarnaKotak.java
@@ -9,6 +9,7 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.Shader;
 import android.graphics.Shader.TileMode;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,15 +20,18 @@ public class AmbilWarnaKotak extends View {
 
 	public AmbilWarnaKotak(final Context context) {
 		this(context, null);
+		this.setSoftwareLayerType();
 	}
 
 	public AmbilWarnaKotak(final Context context, final AttributeSet attrs) {
 		this(context, attrs, 0);
+		this.setSoftwareLayerType();
 	}
 
 	public AmbilWarnaKotak(final Context context, final AttributeSet attrs,
 			final int defStyle) {
 		super(context, attrs, defStyle);
+		this.setSoftwareLayerType();
 	}
 
 	@Override
@@ -51,6 +55,12 @@ public class AmbilWarnaKotak extends View {
 
 	void setHue(final float hue) {
 		this.color[0] = hue;
-		invalidate();
+		this.invalidate();
+	}
+
+	void setSoftwareLayerType() {
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
+			this.setLayerType(LAYER_TYPE_SOFTWARE, null);
+		}
 	}
 }
