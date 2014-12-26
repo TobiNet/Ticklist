a3c36d6fdbbc91c9b1dc200cfe4ff30531af539a
tobias <tobiasmro@googlemail.com>
2014-12-26 22:53:40 +0100

    Gradle

---
+++ build/generated/source/buildConfig/debug/org/tobinet/tick/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.tobinet.tick;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.tobinet.tick";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 9;
+  public static final String VERSION_NAME = "";
+}
